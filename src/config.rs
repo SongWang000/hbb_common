1694ee79338a37af628660bd42725cd2c329d517
SongWang000 <272213517@qq.com>
2025-07-31 16:31:36 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.100.104"];
-pub const RS_PUB_KEY: &str = "miDooIlgNtCaJk9J+ItUfahFxne3ZdfQsoL5OvRtWyE=";
+pub const RS_PUB_KEY: &str = "au44RT3taOJoBstneILy26OxlWcTeTURcDQCJ4bDYP0=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
